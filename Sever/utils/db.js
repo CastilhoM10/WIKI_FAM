d9c67ba7a20e655c67b678379fb19dc786a709d9
Matheus Henrique de Castilho <mathews.castilho@gmail.com>
2024-10-21 16:38:54 -0300

    add: deploy workflow

--- Sever/utils/db.js
+++ Sever/utils/db.js
@@ -3,16 +3,15 @@ import mysql from 'mysql'
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: " ",
+    password: "",  // String vazia se não houver senha
     database: "wiki_fam_new",
 })
 
 con.connect(function(err) {
-    if(err) {
-        console.log("Falha na conexão!")
-    }
-    else{
-        console.log("Conectado!")
+    if (err) {
+        console.log("Falha na conexão!", err.message);  // Adicione err.message para exibir detalhes do erro
+    } else {
+        console.log("Conectado!");
     }
 })
 
